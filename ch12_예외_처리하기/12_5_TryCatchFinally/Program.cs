87860741e3d41e5ce7955e505e82b75462fe3d83
InSung-Na <lht98323@gmail.com>
2023-09-27 17:45:51 +0900

    예외처리문 try_catch_fiinally

---
+++ ch12_예외_처리하기/12_5_TryCatchFinally/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Finally
+{
+    class MainApp
+    {
+        static int Divide(int dividend, int divisor)
+        {
+            try
+            {
+                Console.WriteLine("Divide() 시작");
+                return dividend / divisor;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Divide() 예외 발생");
+                throw e;
+            }
+            finally
+            {
+                Console.WriteLine("Divide() 끝");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("피제수를 입력하세요. : ");
+                int dividend = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("제수를 입력하세요. : ");
+                int divisor = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"{dividend} / {divisor} = {Divide(dividend, divisor)}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("에러 : " + e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine($"에러 : {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("프로그램을 종료합니다.");
+            }
+        }
+    }
+}
---
+++ ch12_예외_처리하기/example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex12_1
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] arr = new int[10];
+                for (int i = 0; i < 10; i++)
+                    arr[i] = i;
+
+                for (int i = 0; i < 11; i++)
+                    Console.WriteLine(arr[i]);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("코드가 종료되었습니다.");
+            }
+        }
+    }
+}
